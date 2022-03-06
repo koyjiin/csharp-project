78e6ba7d760c11cccc44fa3d794f0e6ef01f1ea4
Nicholas <93314425+koyjiin@users.noreply.github.com>
2022-03-06 16:39:55 +0300

    Add files via upload

---
+++ MathModelir/MathModelir/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace MathModelir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(773, 550);
+            this.panel1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(32, 8);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(47, 22);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(32, 43);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(47, 22);
+            this.numericUpDown2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(85, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Длина";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(85, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ширина";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(32, 165);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 22);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Мур";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(32, 140);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(118, 22);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Фон-Нейман";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(32, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 52);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Сгенерировать клетки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(32, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Следующий ход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.numericUpDown4);
+            this.panel2.Controls.Add(this.radioButton2);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(779, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(235, 337);
+            this.panel2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(3, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 35);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Запустить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(116, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 35);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Остановить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(32, 303);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(67, 22);
+            this.numericUpDown4.TabIndex = 14;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1026, 550);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Клеточный автомат";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MathModelir/MathModelir/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathModelir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] cell;
+        bool doc = false;
+        int x, y;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RemoveCellFromPanel();
+
+            x = Convert.ToInt32(numericUpDown1.Value);
+            y = Convert.ToInt32(numericUpDown2.Value);
+            cell = new Button[x, y];
+
+            for(int i = 0; i < x; i++)
+            {
+                for(int q = 0; q < y; q++)
+                {
+                    cell[i, q] = new Button();
+                    cell[i, q].FlatStyle = FlatStyle.Flat;
+                    cell[i, q].Size = new Size(25, 25);
+                    cell[i, q].Location = new Point(i * 25, q * 25);
+                    cell[i, q].Tag = 3;
+                    cell[i, q].BackColor = Color.White;
+                    cell[i, q].Click += new EventHandler(Cell_Click);
+                    panel1.Controls.Add(cell[i, q]);
+                }
+            }
+        }
+
+        private void RemoveCellFromPanel()
+        {
+            if(cell != null)
+            {
+                foreach (var cells in cell)
+                {
+                    if (cells != null)
+                    {
+                        panel1.Controls.Remove(cells);
+                    }
+                }
+            }
+        }
+
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            Button cells = sender as Button;
+
+            if (Convert.ToInt32(cells.Tag) == 3)
+            {
+                cells.Tag = 2;
+                cells.BackColor = Color.Black;
+            }
+
+            else if (Convert.ToInt32(cells.Tag) == 2)
+            {
+                cells.Tag = 3;
+                cells.BackColor = Color.White;
+            }
+
+        }
+
+        int[,] lifecell;
+
+        private void Checked()
+        {
+            for(int i = 0; i < x; i++)
+            {
+                for(int q = 0; q < y; q++)
+                {
+                    if (Convert.ToInt32(cell[i, q].Tag) == 2)
+                    {
+                        if (lifecell[i, q] <= 1 || lifecell[i, q] >= 4)
+                        {
+                            cell[i, q].Tag = 1;
+                            cell[i, q].BackColor = Color.Red;
+                        }
+                    }
+
+                    else if (Convert.ToInt32(cell[i, q].Tag) == 3)
+                    {
+                        if(lifecell[i, q] == 3)
+                        {
+                            cell[i, q].Tag = 0;
+                            cell[i, q].BackColor = Color.Yellow;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(doc)
+            {
+                doc = false;
+
+                for(int i = 0; i < x; i++)
+                {
+                    for(int q = 0; q < y; q++)
+                    {
+                        if (Convert.ToInt32(cell[i, q].Tag) == 0)
+                        {
+                            cell[i, q].Tag = 2;
+                            cell[i, q].BackColor = Color.Black;
+                        }
+
+                        else if (Convert.ToInt32(cell[i, q].Tag) == 1)
+                        {
+                             cell[i, q].Tag = 3;
+                             cell[i, q].BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+
+            else if (!doc)
+            {
+                doc = true;
+
+                if (radioButton1.Checked) //Мур
+                {
+                    lifecell = new int[x, y];
+
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int q = 0; q < y; q++)
+                        {
+                            lifecell[i, q] = 0;
+
+                            if (i - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i - 1 != -1 && q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1 && q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i - 1 != -1 && q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1 && q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+                        }
+                    }
+                    Checked();
+                }
+
+                if (radioButton2.Checked) // Фон-Нейман
+                {
+                    lifecell = new int[x, y];
+
+                    for(int i = 0; i < x; i++)
+                    {
+                        for(int q = 0; q < y; q++)
+                        {
+                            lifecell[i, q] = 0;
+
+                            if(i - 1 != -1)
+                            {
+                                if(Convert.ToInt32(cell[i - 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                        }
+                    }
+                    Checked();
+                }
+            }
+
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (doc)
+            {
+                doc = false;
+
+                for (int i = 0; i < x; i++)
+                {
+                    for (int q = 0; q < y; q++)
+                    {
+                        if (Convert.ToInt32(cell[i, q].Tag) == 0)
+                        {
+                            cell[i, q].Tag = 2;
+                            cell[i, q].BackColor = Color.Black;
+                        }
+
+                        else if (Convert.ToInt32(cell[i, q].Tag) == 1)
+                        {
+                            cell[i, q].Tag = 3;
+                            cell[i, q].BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+
+            else if (!doc)
+            {
+                doc = true;
+
+                if (radioButton1.Checked) //Мур
+                {
+                    lifecell = new int[x, y];
+
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int q = 0; q < y; q++)
+                        {
+                            lifecell[i, q] = 0;
+
+                            if (i - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i - 1 != -1 && q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1 && q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i - 1 != -1 && q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1 && q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+                        }
+                    }
+                    Checked();
+                }
+
+                if (radioButton2.Checked) // Фон-Нейман
+                {
+                    lifecell = new int[x, y];
+                    for (int i = 0; i < x; i++)
+                    {
+                        for (int q = 0; q < y; q++)
+                        {
+                            lifecell[i, q] = 0;
+
+                            if (i - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i - 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q - 1 != -1)
+                            {
+                                if (Convert.ToInt32(cell[i, q - 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (i + 1 <= x - 1)
+                            {
+                                if (Convert.ToInt32(cell[i + 1, q].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                            if (q + 1 <= y - 1)
+                            {
+                                if (Convert.ToInt32(cell[i, q + 1].Tag) == 2)
+                                {
+                                    lifecell[i, q]++;
+                                }
+                            }
+
+                        }
+                    }
+                    Checked();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            button1.Enabled = true;
+            timer1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = Convert.ToInt32(numericUpDown4.Value);
+        }
+
+    }
+}
